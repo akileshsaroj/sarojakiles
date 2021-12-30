bbfaa61269da2fb9c53710b67152e22c25658e83
ravis <akileshsaroj@gmail.com>
2021-12-30 12:28:32 +0530

    First Commit

---
+++ SpringSchedulersEx/src/main/java/com/example/practice/schedulers/Scheduler.java
@@ -0,0 +1,20 @@
+package com.example.practice.schedulers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+	public class Scheduler { 
+	   @Scheduled(fixedRate = 10000)
+	   public void fixedRateSch() {
+	      SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
+
+	      Date now = new Date();
+	      String strDate = sdf.format(now);
+	      System.out.println("Fixed Rate scheduler:: " + strDate);
+	   }
+}
+
